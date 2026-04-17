d960b4a8be2ed70822729797dc7b1cec5651086d
Boss@Baby <109578932+BossBaby11@users.noreply.github.com>
2026-04-17 14:10:25 +0700

    Refactor sorting and searching function

--- main.py
+++ main.py
@@ -74,54 +74,54 @@ def create() :
 
     print("Dokumen berhasil dibuat")
 
-def sorting(data): 
-    if len(data)>1:             #Syarat rekursi jika data tersisa 1 maka rekursi akan berhenti
-        mid = len(data)//2      #proses membagi dua datalist untuk mencari titik tengah
-        lefthalf = data[:mid]   #proses membagi data dimana data dari index awal hingga tengah akan masuk ke bagian kiri
-        righthalf = data[mid:]  #proses membagi data dimana data dari index tengah hingga akhir akan masuk ke bagian kanan
-        sorting(lefthalf)       #proses rekursi yang akan terus membelah data sebelah kiri
-        sorting(righthalf)      #proses rekursi yang akan terus membelah data sebelah kanan
+def sorting(item, sort_by="judul") : 
+    if len(item)>1:             #Syarat rekursi jika data tersisa 1 maka rekursi akan berhenti
+        mid = len(item)//2      #proses membagi dua datalist untuk mencari titik tengah
+        lefthalf = item[:mid]   #proses membagi data dimana data dari index awal hingga tengah akan masuk ke bagian kiri
+        righthalf = item[mid:]  #proses membagi data dimana data dari index tengah hingga akhir akan masuk ke bagian kanan
+        sorting(lefthalf, sort_by)       #proses rekursi yang akan terus membelah data sebelah kiri
+        sorting(righthalf, sort_by)      #proses rekursi yang akan terus membelah data sebelah kanan
         l=0                     #variabel yang akan digunakan untuk memasukkan data sebelah kiri
         r=0                     #variabel yang akan digunakan untuk memasukkan data sebelah kanan
         t=0                     #variabel untuk menempatkan kembali data ke list utama
         
         #proses membandingkan data kiri dan kanan dengan metode ascending
         while l < len(lefthalf) and r < len(righthalf): 
-            if lefthalf[l] <= righthalf[r]:
-                data[t]=lefthalf[l]
+            if lefthalf[l][sort_by] <= righthalf[r][sort_by]:
+                item[t]=lefthalf[l]
                 l=l+1
             else:
-                data[t]=righthalf[r]
+                item[t]=righthalf[r]
                 r=r+1
             t=t+1
         #pengecekan kembali sisa data yang belum terurut disebelah kiri
         while l < len(lefthalf):
-            data[t]=lefthalf[l]
+            item[t]=lefthalf[l]
             l=l+1
             t=t+1
         #pengecekan kembali sisa data yang belum terurut disebelah kanan
         while r < len(righthalf):
-            data[t]=righthalf[r]
+            item[t]=righthalf[r]
             r=r+1
             t=t+1
 
 # Fungsi untuk mencari dokumen di dalam tumpukan dokumen
-def searching() :
-    dokumen = data.data
-    item = input("Masukkan judul buku : ")
+def cari(data, item) :
     first = 0
-    last = len(dokumen)-1
+    last = len(data)-1
     found = False
     while first<=last and not found:
         midpoint = (first + last)//2
-        if dokumen[midpoint] == item:
-            return print("Dokumen ditemukan:", dokumen[midpoint])
+        if data[midpoint]['judul'] == item:
+            return print("Dokumen ditemukan:", data[midpoint]['judul'])
         else:
-            if item < dokumen[midpoint]:
+            if item < data[midpoint]['judul']:
                 last = midpoint-1
             else:
                 first = midpoint+1
-    return "Data tidak ditemukan"
+    return print("Data tidak ditemukan")
+
+
 
 
 def main() :
@@ -150,9 +150,13 @@ def main() :
         case 4 :
             create()
         case 5 :
-            sorting()
-        case 6 :    
-            searching()
+            sorting(data.data)
+            print("Dokumen berhasil diurutkan", data.data)
+        case 6 :
+            inputCari = input("Masukkan nama dokumen yang ingin dicari : ")
+            item_list = list(data.data)
+            sorting(item_list)
+            cari(item_list, inputCari)
         case 7 :
             lanjut = False
         case _ :
